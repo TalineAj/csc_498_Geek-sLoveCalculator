34ca87b9ed7d4d3d3246611ffdc6ce36c7f11323
Taline Abou Jaoude <taline.aboujaoude@lau.edu>
2022-02-14 22:23:59 +0200

    Code working without animation and table

--- app/src/main/java/com/lau/geekslovecalculator/MainActivity.java
+++ app/src/main/java/com/lau/geekslovecalculator/MainActivity.java
@@ -52,8 +52,11 @@ public class MainActivity extends AppCompatActivity {
         Spinner mySpinner = (Spinner)  findViewById(R.id.spinner);
         String language = mySpinner.getSelectedItem().toString();//gets the selected language
         String name_value = name.getText().toString();// gets the name of the user
-        ImageView java = (ImageView) findViewById(R.id.Java);
-        ImageView python = (ImageView) findViewById(R.id.Python);
+        ImageView languageimg = (ImageView) findViewById(R.id.languagelogo);
+        //Was initially planning on using the visibility attribute as learned in class however this means creating 8 Imageviews and setting
+        //their visibility according to the case which does not look efficient , that is why I researched the possibility of setting image in java
+        //and there was a way to do so which I used here
+        Resources res = getResources();
         Random num =  new Random();
         int match = num.nextInt(100);
 
@@ -63,29 +66,57 @@ public class MainActivity extends AppCompatActivity {
             {
                 case "Java":
                 {
-                    //do the same for the other ones
-                    python.setVisibility(View.INVISIBLE);
-                    java.setVisibility(View.VISIBLE);
-                    java.animate().rotation(360).setDuration(2000);
+
+                    Drawable java_logo = ResourcesCompat.getDrawable(res, R.drawable.java, null);
+                    languageimg.setImageDrawable(java_logo);
+//                    languageimg.animate().rotation(360).setDuration(2000);
                 }
                     break;
                 case "JavaScript":
+                {   Drawable js_logo = ResourcesCompat.getDrawable(res, R.drawable.js, null);
+                    languageimg.setImageDrawable(js_logo);
+//                    languageimg.animate().translationX(200).setDuration(1000);
                     break;
+                }
                 case "Python":
-                    java.setVisibility(View.INVISIBLE);
-                    python.setVisibility(View.VISIBLE);
-                    python.animate().rotation(360).setDuration(2000);
+                {   Drawable python_logo = ResourcesCompat.getDrawable(res, R.drawable.python, null);
+                    languageimg.setImageDrawable(python_logo);
+//                    languageimg.animate().rotation(3600).setDuration(2000);
                     break;
+                }
+
                 case "C":
+                {
+                    Drawable C_logo = ResourcesCompat.getDrawable(res, R.drawable.c, null);
+                    languageimg.setImageDrawable(C_logo);
+//                    languageimg.animate().rotation(3300).setDuration(1000);
                     break;
+                }
                 case "C++":
+                {
+                    Drawable Cpp = ResourcesCompat.getDrawable(res, R.drawable.cpp, null);
+                    languageimg.setImageDrawable(Cpp);
+//                    languageimg.animate().rotation(360).setDuration(1000);
                     break;
+                }
                 case "C#":
+                {   Drawable Csharp = ResourcesCompat.getDrawable(res, R.drawable.csharp, null);
+                    languageimg.setImageDrawable(Csharp);
+//                    languageimg.animate().scaleY(10).setDuration(1000);
                     break;
+                }
                 case "PHP":
+                {   Drawable php = ResourcesCompat.getDrawable(res, R.drawable.php, null);
+                    languageimg.setImageDrawable(php);
+//                    languageimg.animate().translationX(-100).setDuration(1000);
                     break;
+                }
                 case "R":
+                {   Drawable r = ResourcesCompat.getDrawable(res, R.drawable.r, null);
+                    languageimg.setImageDrawable(r);
+//                    languageimg.animate().translationZ(10).setDuration(1000);
                     break;
+                }
             }
 
 
